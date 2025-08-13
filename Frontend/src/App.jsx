bf31d7995f933c58d58e227c6673770d76b2cc75
Abhii01 <avee.abhishek220@gmail.com>
2025-08-13 17:15:50 +0530

    code changes in App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './component/Footer';
 import Nav from './component/Nav';
-import { CategoriesProvider } from './utilis/CategoriesContext';
 
 function App() {
   return (
-    <CategoriesProvider>
+    <>
       <Nav />
       <Outlet />
       <Footer />
-    </CategoriesProvider>
+    </>
   );
 }
 
